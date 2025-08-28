b8ec84f926765f59aeb81d354e22695073c95c7d
Nuno Cruces <ncruces@users.noreply.github.com>
2025-08-29 00:47:36 +0200

    Split file

--- set_test.go
+++ set_test.go
@@ -6,41 +6,76 @@ import (
 )
 
 func TestUnion(t *testing.T) {
-	t1 := FromSorted([]Entry[int, int]{{1, 1}, {2, 2}, {3, 3},})
-	t2 := FromSorted([]Entry[int, int]{{2, 22}, {3, 33}, {4, 4},})
-	got := Union(t1, t2).Entries()
-	want := []Entry[int, int]{{1, 1}, {2, 2}, {3, 3}, {4, 4}}
-	if !slices.Equal(got, want) {
-		t.Errorf("Union() = %v, want %v", got, want)
+	var t1, t2 *Tree[int, string]
+	t1 = t1.Put(1, "one").Put(2, "two").Put(3, "three").Put(5, "five")
+	t2 = t2.Put(0, "zero").Put(2, "").Put(4, "four")
+
+	if a := Union(t1, nil); a != t1 {
+		t.Errorf("%p ≠ %p", a, t1)
+	}
+	if a := Union(nil, t2); a != t2 {
+		t.Errorf("%p ≠ %p", a, t2)
+	}
+
+	var j int
+	for i, v := range Union(t1, t2).Ascend() {
+		if i != j {
+			t.Errorf("%d ≠ %d", i, j)
+		}
+		if v == "" {
+			t.Error()
+		}
+		j++
 	}
 }
 
 func TestIntersection(t *testing.T) {
-	t1 := FromSorted([]Entry[int, int]{{1, 1}, {2, 2}, {3, 3},})
-	t2 := FromSorted([]Entry[int, int]{{2, 22}, {3, 33}, {4, 4},})
-	got := Intersection(t1, t2).Entries()
-	want := []Entry[int, int]{{2, 2}, {3, 3}}
-	if !slices.Equal(got, want) {
-		t.Errorf("Intersection() = %v, want %v", got, want)
+	var t1, t2 *Tree[int, string]
+	t1 = t1.Put(1, "one").Put(2, "two").Put(3, "three").Put(5, "five")
+	t2 = t2.Put(0, "zero").Put(2, "").Put(4, "four")
+
+	var out []int
+	for i, v := range Intersection(t1, t2).Ascend() {
+		out = append(out, i)
+		if v == "" {
+			t.Error()
+		}
+	}
+	if !slices.Equal(out, []int{2}) {
+		t.Error(out)
 	}
 }
 
 func TestDifference(t *testing.T) {
-	t1 := FromSorted([]Entry[int, int]{{1, 1}, {2, 2}, {3, 3},})
-	t2 := FromSorted([]Entry[int, int]{{2, 22}, {3, 33}, {4, 4},})
-	got := Difference(t1, t2).Entries()
-	want := []Entry[int, int]{{1, 1}}
-	if !slices.Equal(got, want) {
-		t.Errorf("Difference() = %v, want %v", got, want)
+	var t1, t2 *Tree[int, string]
+	t1 = t1.Put(1, "one").Put(2, "two").Put(3, "three").Put(5, "five")
+	t2 = t2.Put(0, "zero").Put(2, "").Put(4, "four")
+
+	var out []int
+	for i, v := range Difference(t1, t2).Ascend() {
+		out = append(out, i)
+		if v == "" {
+			t.Error()
+		}
+	}
+	if !slices.Equal(out, []int{1, 3, 5}) {
+		t.Error(out)
 	}
 }
 
 func TestSymmetricDifference(t *testing.T) {
-	t1 := FromSorted([]Entry[int, int]{{1, 1}, {2, 2}, {3, 3},})
-	t2 := FromSorted([]Entry[int, int]{{2, 22}, {3, 33}, {4, 4},})
-	got := SymmetricDifference(t1, t2).Entries()
-	want := []Entry[int, int]{{1, 1}, {4, 4}}
-	if !slices.Equal(got, want) {
-		t.Errorf("SymmetricDifference() = %v, want %v", got, want)
+	var t1, t2 *Tree[int, string]
+	t1 = t1.Put(1, "one").Put(2, "two").Put(3, "three").Put(5, "five")
+	t2 = t2.Put(0, "zero").Put(2, "").Put(4, "four")
+
+	var out []int
+	for i, v := range SymmetricDifference(t1, t2).Ascend() {
+		out = append(out, i)
+		if v == "" {
+			t.Error()
+		}
+	}
+	if !slices.Equal(out, []int{0, 1, 3, 4, 5}) {
+		t.Error(out)
 	}
 }
